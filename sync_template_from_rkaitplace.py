d09936a3b320058d91c3d36e7ea1eedbfe644f75
Kaibin Bao <kaibin.bao@baihu.de>
2022-04-03 22:56:13 +0200

    a lot of bugfixes

--- sync_template_from_rkaitplace.py
+++ sync_template_from_rkaitplace.py
@@ -8,22 +8,23 @@ import math
 
 def update_from_upstream(original_url, filename):
 	# download template from upstream
-	original_url = 
 	response = requests.get(original_url)
-	b64pngregex = re.compile(r"^.*\"data:image/png;base64,([^\"]*).*$")
+	b64pngregex = re.compile(r"^.*data:image/png;base64,([^\"]*).*$")
 	img = None
 	for line in response.text.split("\n"):
 		b64pngmatch = b64pngregex.match(line)
 		if b64pngmatch:
 			print("FOUND REFERENCE")
-			img = Image.open(BytesIO(binascii.a2b_base64(b64pngmatch.group(1))))
+			img = Image.open(BytesIO(binascii.a2b_base64(b64pngmatch.group(1)))).convert("RGBA")
+			if img.size[0] > 80:
+				img = img.resize((img.size[0] // 10, img.size[1] // 10), Image.NEAREST)
 			break
 
 	if not img:
-		img = Image.open(filename)
+		img = Image.open(filename).convert("RGBA")
 
 	# fix colors
-	palette = [0x6d001a, 0xBE0039, 0xFF4500, 0xFFA800, 0xFFD635, 0xfff8b8, 0x00A368, 0x00CC78, 0x7EED56, 0x00756F, 0x009EAA, 0x00ccc0, 0x2450A4, 0x3690EA, 0x51E9F4, 0x493AC1, 0x6A5CFF, 0x94b3ff, 0x811E9F, 0xB44AC0, 0xe4abff, 0xde107f, 0xFF3881, 0xFF99AA, 0x6D482F, 0x9C6926, 0xffb470, 0x000000, 0x515252, 0x898D90, 0xD4D7D9, 0xFFFFFF]
+	palette = np.array([0x6d001a, 0xBE0039, 0xFF4500, 0xFFA800, 0xFFD635, 0xfff8b8, 0x00A368, 0x00CC78, 0x7EED56, 0x00756F, 0x009EAA, 0x00ccc0, 0x2450A4, 0x3690EA, 0x51E9F4, 0x493AC1, 0x6A5CFF, 0x94b3ff, 0x811E9F, 0xB44AC0, 0xe4abff, 0xde107f, 0xFF3881, 0xFF99AA, 0x6D482F, 0x9C6926, 0xffb470, 0x000000, 0x515252, 0x898D90, 0xD4D7D9, 0xFFFFFF])
 
 	def l2diff(a,b):
 		s = ((a & 0xff) - (b & 0xff))**2
@@ -32,18 +33,18 @@ def update_from_upstream(original_url, filename):
 		return s
 
 	if img:
+		img_new = Image.new('RGBA', (img.size[0], img.size[1]))
 		for x in range(img.size[0]):
 			for y in range(img.size[1]):
 				p = img.getpixel((x, y))
 				original_color = (p[0] << 16) + (p[1] << 8) + p[2]
-				if original_color not in palette:
-					palette_diff = [l2diff(original_color, pc) for pc in palette]
-					palette_i = np.argmin(palette_diff)
-					c = palette[palette_i]
-					print(hex(original_color), "->", hex(c), "at", x, y)
-					img.putpixel((x, y), (c>>16 & 0xff, c>>8 & 0xff, c & 0xff, 0xff))
-		img.save(filename)
-		return img
+				palette_diff = [l2diff(original_color, pc) for pc in palette]
+				palette_i = np.argmin(palette_diff)
+				c = palette[palette_i]
+				print(hex(original_color), "->", hex(c), "at", x, y)
+				img_new.putpixel((x, y), ((c>>16) & 0xff, (c>>8) & 0xff, c & 0xff, 0xff))
+		img_new.save(filename)
+		return img_new
 
 # create overlay
 
