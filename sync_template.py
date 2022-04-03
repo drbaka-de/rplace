01bc316fd3abb0a0edca14aaef2dc3e8b881d436
Kaibin Bao <kaibin.bao@baihu.de>
2022-04-03 14:42:21 +0200

    brace yourself for the koeri

--- sync_template.py
+++ sync_template.py
@@ -4,14 +4,20 @@ from io import BytesIO
 
 img = Image.open("reference.png")
 img = img.resize((img.size[0] * 3, img.size[1] * 3), Image.NEAREST)
+img2 = Image.open("reference_koeri.png")
+img2 = img2.resize((img2.size[0] * 3, img2.size[1] * 3), Image.NEAREST)
 
 mask_url = "https://media.discordapp.net/attachments/267492253168173056/959625681141104700/mask.png"
 response = requests.get(mask_url)
 mask_i = Image.open(BytesIO(response.content))
 mask = Image.new("1", (3000, 3000), 0)
 mask.paste(mask_i)
+mask2 = Image.new("1", (3000*2, 3000), 0)
+mask2.paste(mask_i, (0, 0, 3000, 3000))
+mask2.paste(mask_i, (3000, 0))
 
 tl = (760 * 3, 521  * 3) # top left corner
+tl2 = (1334 * 3, 754  * 3) # top left corner of img2
 
 final_img = Image.new('RGBA', (3000, 3000))
 unmasked_img = Image.new('RGBA', (3000, 3000))
@@ -19,3 +25,9 @@ unmasked_img.paste(img, tl)
 final_img = Image.composite(final_img, unmasked_img, mask)
 final_img.save("kait_overlay.png")
 
+final_img2 = Image.new('RGBA', (3000*2, 3000))
+unmasked_img2 = Image.new('RGBA', (3000*2, 3000))
+unmasked_img2.paste(img, tl)
+unmasked_img2.paste(img2, tl2)
+final_img2 = Image.composite(final_img2, unmasked_img2, mask2)
+final_img2.save("kait_koeri_overlay.png")
