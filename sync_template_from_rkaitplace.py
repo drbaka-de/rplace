e23f83e645a1ab901ad2ec65d687b35b0a22debf
Linus <linuscodes@scobe.de>
2022-04-03 23:21:53 +0200

    reorganize sync script

--- sync_template_from_rkaitplace.py
+++ sync_template_from_rkaitplace.py
@@ -1,67 +1,61 @@
+from distutils.file_util import write_file
 from PIL import Image, ImageOps
 import requests
 from io import BytesIO
 import re
 import binascii
 import numpy as np
-import math
+from sync_template import main
 
-def update_from_upstream(original_url, filename):
-	# download template from upstream
-	response = requests.get(original_url)
+palette = [0x6d001a, 0xBE0039, 0xFF4500, 0xFFA800, 0xFFD635, 0xfff8b8, 0x00A368, 0x00CC78, 0x7EED56, 0x00756F, 0x009EAA, 0x00ccc0, 0x2450A4, 0x3690EA, 0x51E9F4, 0x493AC1, 0x6A5CFF, 0x94b3ff, 0x811E9F, 0xB44AC0, 0xe4abff, 0xde107f, 0xFF3881, 0xFF99AA, 0x6D482F, 0x9C6926, 0xffb470, 0x000000, 0x515252, 0x898D90, 0xD4D7D9, 0xFFFFFF]
+
+def l2diff(a,b):
+	s = ((a & 0xff) - (b & 0xff))**2
+	s += (((a>>8) & 0xff) - ((b>>8) & 0xff))**2
+	s += (((a>>16) & 0xff) - ((b>>16) & 0xff))**2
+	return s
+
+def fix_colors(img):
+	img = img.convert("RGBA")
+	print("fix colors")
+	print(img)
+	for x in range(img.size[0]):
+		for y in range(img.size[1]):
+			p = img.getpixel((x, y))
+			original_color = (p[0] << 16) + (p[1] << 8) + p[2]
+			palette_diff = [l2diff(original_color, pc) for pc in palette]
+			palette_i = np.argmin(palette_diff)
+			c = palette[palette_i]
+			print(hex(original_color), "->", hex(c), "at", x, y)
+			img.putpixel((x, y), (c>>16 & 0xff, c>>8 & 0xff, c & 0xff, 0xff))
+	return img
+
+
+# download template from upstream
+
+def fetch_reference(url):
+	response = requests.get(url)
 	b64pngregex = re.compile(r"^.*data:image/png;base64,([^\"]*).*$")
-	img = None
 	for line in response.text.split("\n"):
 		b64pngmatch = b64pngregex.match(line)
 		if b64pngmatch:
-			print("FOUND REFERENCE")
-			img = Image.open(BytesIO(binascii.a2b_base64(b64pngmatch.group(1)))).convert("RGBA")
-			if img.size[0] > 80:
-				img = img.resize((img.size[0] // 10, img.size[1] // 10), Image.NEAREST)
-			break
+			print("FOUND REFERENCE in " + url)
+			return Image.open(BytesIO(binascii.a2b_base64(b64pngmatch.group(1))))
+
+def update_reference(url, file):
+	img = fetch_reference(url)
 
 	if not img:
-		img = Image.open(filename).convert("RGBA")
-
-	# fix colors
-	palette = np.array([0x6d001a, 0xBE0039, 0xFF4500, 0xFFA800, 0xFFD635, 0xfff8b8, 0x00A368, 0x00CC78, 0x7EED56, 0x00756F, 0x009EAA, 0x00ccc0, 0x2450A4, 0x3690EA, 0x51E9F4, 0x493AC1, 0x6A5CFF, 0x94b3ff, 0x811E9F, 0xB44AC0, 0xe4abff, 0xde107f, 0xFF3881, 0xFF99AA, 0x6D482F, 0x9C6926, 0xffb470, 0x000000, 0x515252, 0x898D90, 0xD4D7D9, 0xFFFFFF])
-
-	def l2diff(a,b):
-		s = ((a & 0xff) - (b & 0xff))**2
-		s += (((a>>8) & 0xff) - ((b>>8) & 0xff))**2
-		s += (((a>>16) & 0xff) - ((b>>16) & 0xff))**2
-		return s
+		img = Image.open(file)
 
 	if img:
-		img_new = Image.new('RGBA', (img.size[0], img.size[1]))
-		for x in range(img.size[0]):
-			for y in range(img.size[1]):
-				p = img.getpixel((x, y))
-				original_color = (p[0] << 16) + (p[1] << 8) + p[2]
-				palette_diff = [l2diff(original_color, pc) for pc in palette]
-				palette_i = np.argmin(palette_diff)
-				c = palette[palette_i]
-				print(hex(original_color), "->", hex(c), "at", x, y)
-				img_new.putpixel((x, y), ((c>>16) & 0xff, (c>>8) & 0xff, c & 0xff, 0xff))
-		img_new.save(filename)
-		return img_new
+		img = fix_colors(img)
+		img.save(file)
 
-# create overlay
 
-kit_image = update_from_upstream("https://rkaitplace.netlify.app/", "reference.png")
-kit_image = kit_image.resize((kit_image.size[0] * 3, kit_image.size[1] * 3), Image.NEAREST)
+if __name__ == "__main__":
+	update_reference("https://rkaitplace.netlify.app/", "reference.png")
+	update_reference("https://rkaitplace.netlify.app/koeri.html", "reference_koeri.png")
 
-koeri_image = update_from_upstream("https://rkaitplace.netlify.app/koeri.html", "reference_koeri.png")
-koeri_image = koeri_image.resize((koeri_image.size[0] * 3, koeri_image.size[1] * 3), Image.NEAREST)
-
-mask = Image.open("mask.png").convert("1")
-
-top_left_kit = (760 * 3, 521 * 3)
-top_left_koeri = (601 * 3, 1887 * 3)
-
-empty = Image.new('RGBA', (6000, 6000))
-overlay = Image.new('RGBA', (6000, 6000))
-overlay.paste(kit_image, top_left_kit)
-overlay.paste(koeri_image, top_left_koeri)
-overlay.paste(empty, None, mask)
-overlay.save("overlay.png")
+	# run other script
+	main()
