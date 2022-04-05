049b2c3d076ac3ee25d708bf73410a902ebc6f21
Kaibin Bao <kaibin.bao@baihu.de>
2022-04-05 07:56:34 +0200

    forgot to resize

--- sync_template_b4_whiteout.py
+++ sync_template_b4_whiteout.py
@@ -5,6 +5,7 @@ from io import BytesIO
 def main():
     mask = Image.open("mask.png").convert("1")
     b4_the_whiteout = Image.open("cxjalbw3hlr81.png").convert("RGBA")
+    b4_the_whiteout = b4_the_whiteout.resize((b4_the_whiteout.size[0] * 3, b4_the_whiteout.size[1] * 3), Image.NEAREST)
 
     empty = Image.new('RGBA', (6000, 6000))
     b4_the_whiteout.paste(empty, None, mask)
